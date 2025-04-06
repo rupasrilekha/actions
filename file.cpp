10d4420491cecfffc60c40b8048f6247141dfd1b
Kandula Rupa Sri Lekha <142570907+rupasrilekha@users.noreply.github.com>
2025-04-06 10:18:25 +0530

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl
+
+    int unusedVariable = 42;  // Unused variable
+
+    if (true) {
+        std::cout << "This should be indented properly" << std::endl;
+    }
+    
+    return 0
+}
