fa3a80a6df98699a13e6abcdc0809c0074caeb1f
Kandula Rupa Sri Lekha <142570907+rupasrilekha@users.noreply.github.com>
2025-04-06 10:25:50 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,16 +1,17 @@
-// Copyright 2025 Rupa srilekha
-// This file is part of the  project.
+// Copyright 2025 <Your Name or Company>
+// This file is part of the <Your Project Name> project.
 
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    int unusedVariable = 42;  
+    int unusedVariable = 42;  // Unused variable
 
+    // The following line was too long and now broken into multiple lines
     if (true) {
-        std::cout << "This should be indented properly" << unusedVariable << std::endl;
+        std::cout << "This should be indented properly" << std::endl;
     }
-    
+
     return 0;
 }
