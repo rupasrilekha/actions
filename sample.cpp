b6b79826066ada0a59ca47e041234c88fe828d36
Kandula Rupa Sri Lekha <142570907+rupasrilekha@users.noreply.github.com>
2025-04-14 20:16:51 +0530

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,12 +1,12 @@
 // Copyright 2025 Rupa
-// #include <iostream>
+
 
 int main() {
     int x = 0;
     for (int i = 0; i < 10; i++) {
         x += i;
     }
-    std::cout << "Sum: " << x << std::endl;
-    return 0;
+    std::cout << "Sum: " << x << std::endl
+    return 0
 }
 
