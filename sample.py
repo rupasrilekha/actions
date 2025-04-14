a654df682020eab4d714cffaa5b90bf37f3927b0
Kandula Rupa Sri Lekha <142570907+rupasrilekha@users.noreply.github.com>
2025-04-14 19:48:29 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,10 @@
-// Copyright 2025 Rupa
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
-    int x = 0;
-    for (int i = 0; i < 10; i++) {
-        x += i;
-    }
-    std::cout << "Sum: " << x << std::endl;
-    return 0;
+int main(){
+int x=0;
+for(int i=0;i<10;i++){
+x+=i;
 }
+cout<<"Sum: "<<x<<endl;
+return 0;}
